c430b42e19f541d97b7e00629ca84d487899c5bf
JavaScript <javascript@Admins-iMac-105.local>
2020-01-14 20:50:44 +0200

    curs

--- Projects Nicolae Marius/JavaScript/spanzuratoarea/hangman.js
+++ Projects Nicolae Marius/JavaScript/spanzuratoarea/hangman.js
@@ -12,12 +12,52 @@ var game = {
   },
   tryLetter: function(letter) {
     this.triedLetters.push(letter);
-    if (!this.word.split("").include(letter)) {
+    if (!this.word.split("").includes(letter)) {
       this.triesLeft--;
     }
   },
   isDead: function() {
     return this.triesLeft == 0;
   },
-  isWinner: function() {}
+  isWinner: function() {
+    return this.word
+      .split("")
+      .every(letter => this.triedLetters.includes(letter));
+  }
 };
+
+function renderLetters() {
+  document.getElementById("letters").innerText = game.triedLetters.join(" ");
+}
+
+function renderWord() {
+  var text = game.word
+    .split("")
+    .map(letter => (game.triedLetters.includes(letter) ? letter : "_"))
+    .join(" ");
+  document.getElementById("word").innerText = text;
+}
+
+function renderDrawing() {
+  let parts = ["head", "body", "leftArm", "rightArm", "leftLeg", "rightLeg"];
+  let visible = parts.slice(0, 6 - game.triesLeft);
+  let invisible = parts.slice(6 - game.triesLeft);
+  visible.forEach(
+    name => (document.getElementById(name).style.visibility = "visible")
+  );
+}
+
+function renderView() {
+  renderLetters();
+  renderWord();
+  renderDrawing();
+}
+
+window.addEventListener("keydown", function(event) {
+  if (game.isDead() || game.isWinner()) return;
+  game.tryLetter(event.key);
+  renderView();
+});
+
+game.start();
+renderView();
