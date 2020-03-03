ff957c2743aa0b008e1ab13916ade04d004842d9
oraNge-M112 <nicolae.marius.761@gmail.com>
2020-03-03 22:58:46 +0200

    pauza

---
+++ Projects Nicolae Marius/JavaScript/Memory Game/memory.js
@@ -0,0 +1,42 @@
+const animals = [
+  "turtle",
+  "snail",
+  "corgi",
+  "parrot",
+  "rabbit",
+  "bear",
+  "panda",
+  "fish",
+  "shark",
+  "pig",
+  "crab",
+  "rhinoceros"
+];
+
+const url = "https://png.icons8.com/100/000000/color/";
+
+var images = animals.map(x => url + x + ".png");
+
+// images = images.concat(images);
+
+function suffle(array) {
+  let i = 0;
+  for (i = 0; i < array.length; i++) {
+    let j = Math.floor(Math.random() * array.length);
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+suffle(images);
+console.log(images);
+
+let i;
+let content = "";
+for (i = 0; i < images.length; i++) {
+  let html = `<li><img src="${images[i]}"><li>`;
+  content += html;
+}
+
+document.querySelector("ul").innerHTML = content;
