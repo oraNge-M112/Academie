19b8078b9f784bc72bf2a65949e0d9c23d72d3f8
oraNge-M112 <marius.nicolae.761@gmail.com>
2020-04-13 21:44:42 +0300

    chuck home

--- Advanced JS/vue/chuck/chuck.js
+++ Advanced JS/vue/chuck/chuck.js
@@ -1,14 +1,21 @@
 var app = new Vue({
-    el: "#app",
-    data: {
-        fact: "Loading.."
+  el: "#app",
+  data: {
+    fact: "To get the first fact press the button below",
+  },
+  methods: {
+    getAnother: function () {
+      axios
+        .get("http://api.icndb.com/jokes/random")
+        .then((response) => (this.fact = response.data.value.joke))
+        .catch(
+          (error) =>
+            (this.fact = error + "Error while loading. Please try again.")
+        );
     },
-    // metoda ce se apeleaza cand porneste aplicatia
-    mounted() {
-        axios
-            .get("http://api.icndb.com/jokes/random")
-            .then(response => this.fact = response.data.value.joke)
-            .catch(error => this.fact = error + "Error while loading. Please try again.");
-    }
+  },
+  // metoda ce se apeleaza cand porneste aplicatia
+  mounted() {
+    getAnother();
+  },
 });
-	
