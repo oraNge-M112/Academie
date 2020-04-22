0a1f675c0d79274f36b2df1722cb209081469c80
oraNge-M112 <marius.nicolae.761@gmail.com>
2020-04-22 11:18:52 +0300

    todo academie

--- vue/todo/todo.js
+++ vue/todo/todo.js
@@ -1,3 +1,27 @@
+class Todo {
+	constructor(title) {
+		this.title = title;
+		this.isDone = false; 
+	}
+	switchDone() {
+		this.isDone()
+	}
+}
+
+Vue.component('task', {
+	template: `
+		<li :class="{ done: isDone }"> 
+			<div class="title"> {{ title }}</div>
+			<div class="controls">
+				<div class="check" @click:"$emit('checked')">✓</div>
+				<div class="delete" @click:"$emit('deleted')">❌</div>
+			</div>
+		</li>
+	`,
+	props: ['title', 'isDone']
+});
+
+
 var app = new Vue({
 	el: '#app',
 	data: {
