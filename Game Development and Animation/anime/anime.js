4c387b8b959395a99f3fab64281a4eed3c078614
Marius-N <marius.nicolae.761@gmail.com>
2020-07-06 16:37:51 +0300

    animatii rezolvare

--- Game Development and Animation/anime/anime.js
+++ Game Development and Animation/anime/anime.js
@@ -1,8 +1,18 @@
 anime({
-  targets: 'div.square',
-  translateX: 250,
-  duration: 3000,
+	targets: 'div.square',
+	translateX: 250,
+	duration: 3000,
+	loop: true,
+	direction: 'alternate',
+	delay: anime.stagger(100),
+})
+
+anime({
+  targets: 'path',
+  duration: 2000,
+  strokeDashoffset: [anime.setDashoffset, 0],
+  delay: function(el, i) { return i * 250 },
+  easing: 'easeInOutSine',
   loop: true,
-  direction: 'alternate',
-  delay: anime.stagger(100)
-});
+  direction: 'alternate'
+})
